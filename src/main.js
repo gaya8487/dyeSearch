3f3534b5ae5bbf8b7c9dfe66f9891047c5a1188e
gaya8487 <qltkd8487@gmail.com>
2022-07-25 12:16:27 +0900

    수정

--- src/main.js
+++ src/main.js
@@ -10,10 +10,18 @@ import "bootstrap"
 import tooltip from "./directives/tooltip.js";
 import "./assets/tooltip.css";
 
+//GA
+import VueGtag from 'vue-gtag-next'
+
 
 const app = createApp(App);
 // app.directive's first argument is the directive's name you will use
 // it can be whatever you wish
 app.directive("tooltip", tooltip);
+
+app.use(VueGtag,{
+    property : {id : 'G-BP0X6SK19K'}
+});
+
 app.mount("#app");
 
